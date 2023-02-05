b40031ecb65b450cb6c1ed3c1e679cb02654fffb
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-05 19:02:13 +0300

    Вычисляемые свойства в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -3,82 +3,169 @@ export default {
   name: 'App',
   data() {
     return {
-      text: 'Абзац',
+      cosnt_tomato: 69,
+      cosnt_cucumbers: 48,
+      cosnt_cabbage: 45,
+
+      amount_tomato: 3,
+      amount_cucumbers: 5,
+      amount_cabbage: 10,
     }
+
+  },
+
+  computed: {
+    price_totmato: function () {
+      return this.cosnt_tomato * this.amount_tomato;
+    },
+
+    price_cucumbers: function () {
+      return this.cosnt_cucumbers * this.amount_cucumbers;
+    },
+
+    price_cabbage: function () {
+      return this.cosnt_cabbage * this.amount_cabbage;
+    },
+
+    price_total: function () {
+      return this.price_cabbage + this.price_cucumbers + this.price_totmato;
+    },
   },
   methods: {
-
-    task2: function () {
-      this.text = 'Параграф';
+    reduce: function () {
+      this.amount_tomato -= 1;
+      this.amount_cucumbers -= 1;
+      this.amount_cabbage -= 1;
     },
 
-    task3: function () {
-      this.text = 'Текст';
+    increase: function () {
+      this.amount_tomato += 1;
+      this.amount_cucumbers += 1;
+      this.amount_cabbage += 1;
     },
-
-    task4: function () {
-      this.text = 'Заголовок';
-    }
   }
 }
 </script>
 
 <template>
+  <table>
 
-<p class="box-red">
-  <span> {{ text }} </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui ea repellendus ex, nihil quos sapiente dolores expedita assumenda ipsa blanditiis est magnam animi aut, in mollitia vitae repudiandae tempore!
-  Lorem ipsum  sit amet consectetur adipisicing elit. Amet qui ea repellendus ex, nihil quos sapiente dolores expedita assumenda ipsa blanditiis est magnam animi aut, in mollitia vitae repudiandae tempore!
-</p>
+    <caption>
+      <h1>Расчет</h1>
+    </caption>
 
-<p class="box-blue">
-  <span> {{ text }} </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui ea repellendus ex, nihil quos sapiente dolores expedita assumenda ipsa blanditiis est magnam animi aut, in mollitia vitae repudiandae tempore!
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet qui ea repellendus ex, nihil quos sapiente dolores expedita assumenda ipsa blanditiis est magnam animi aut, in mollitia vitae repudiandae tempore!
-</p>
+    <colgroup>
+      <col span="1" style="background:Khaki">
+      <col style="background-color:LightCyan">
+      <col style="background-color:rgb(255, 224, 224)">
+      <col style="background-color:#B5FFA0">
+      <col style="background-color:#FFD7AF">
+      <col style="background-color:#FFBEFD">
+    </colgroup>
 
-<button class="link" @click="task2">Изменить свойство</button>
-<button class="link" @click="task3">Изменить свойство</button>
-<button class="link" @click="task4">Изменить свойство</button>
+    <thead>
+
+      <tr>
+        <th>№ п/п</th>
+        <th>Наименование товара</th>
+        <th>Ед. изм.</th>
+        <th>Количество</th>
+        <th>Цена за ед. изм., руб.</th>
+        <th>Стоимость, руб.</th>
+      </tr>
+
+    </thead>
+    <tbody>
+
+      <tr>
+        <td>1.</td>
+        <td>Томаты свежие</td>
+        <td>кг</td>
+        <td>{{ amount_tomato }}</td>
+        <td>{{ cosnt_tomato }}</td>
+        <td>{{ price_totmato }}</td>
+      </tr>
+
+      <tr>
+        <td>2.</td>
+        <td>Огурцы свежие</td>
+        <td>кг</td>
+        <td>{{ amount_cucumbers }}</td>
+        <td>{{ cosnt_cucumbers }}</td>
+        <td>{{ price_cucumbers }}</td>
+      </tr>
+
+      <tr>
+        <td>3.</td>
+        <td>Капуста свежая</td>
+        <td>кг</td>
+        <td>{{ amount_cabbage }}</td>
+        <td>{{ cosnt_cabbage }}</td>
+        <td>{{ price_cabbage }}</td>
+      </tr>
+
+    </tbody>
+
+
+    <tfoot>
+
+      <tr style="background-color: #B8D8E1;">
+        <td colspan="5" style="text-align:right" laststr="last">ИТОГО:</td>
+        <td>{{ price_total }}</td>
+      </tr>
+
+    </tfoot>
+
+  </table>
+  <button class="link" @click="increase">Добавить кол-во овощей</button>
+  <button class="link" @click="reduce">Уменьшить кол-во овощей</button>
 
 </template>
 
 <style>
-
 body {
-  margin: 60px;
-  font-family: Arial, Helvetica, sans-serif;
+  margin: 30px;
 }
 
-.box-red {
-  background-color: #fc251e;
-  display: inline-block;
-  padding: 10px 40px;
-  border-radius: 40px;
-  font-weight: bold;
-  margin: 20px;
+table,
+tr,
+th,
+td {
+  border: 1px solid #000;
+  border-collapse: collapse;
+
 }
 
-.box-blue {
-  background-color: #1e22fc;
-  display: inline-block;
-  padding: 10px 40px;
-  border-radius: 40px;
-  font-weight: bold;
-  margin: 20px;
+tr,
+td,
+th {
+  border: 1px dashed #000;
 }
 
-span {
-  color: #30c702;
-  background: #000;
-  font-size: 15px;
-  padding: 4px 10px;
-  margin-right: 3px;
-  border-radius: 40px;
+table {
+  width: 100%;
+  text-align: center;
+
+}
+
+th {
+  padding: 10px;
+
+}
+
+td {
+  padding: 5px 7px;
+
+}
+
+tr:hover td {
+  background: #e8edff;
 }
 
 
 .link {
-  color: #30c702;
-  background: #000;
+  color: #FFBEFD;
+  background: #548cc0;
   font-weight: bold;
   font-size: 20px;
   padding: 10px 40px;
