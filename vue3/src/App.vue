ccef29fc7a82724a7b83df6f3b36f6474b43627c
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-06 00:52:09 +0300

    Тогглинг элементов в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -3,25 +3,26 @@ export default {
   name: 'App',
   data() {
     return {
-      visible1: false,
+      visible1: true,
       visible2: true,
+      visible3: true,
     }
   },
 
   computed: {
   },
+
   methods: {
-    showItem1: function () {
-      this.visible1 = true;
+    toggle1: function () {
+      this.visible1 =  !this.visible1;
     },
-
-    hideItem2: function () {
-      this.visible2 = false;
-    }, 
-
-    showItem2: function () {
-      this.visible2 = true;
+    toggle2: function () {
+      this.visible2=  !this.visible2;
     },
+    toggle3: function () {
+      this.visible3 =  !this.visible3;
+    }
+
   }
 }
 </script>
@@ -31,23 +32,18 @@ export default {
 <p class="box" v-if="visible1">
   Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Обеспечивает переписали запятых свою имени журчит рыбными то своих семантика безорфографичный что коварных запятой буквоград строчка над, большого залетают. Журчит.
 </p>
-<button class="link" @click="showItem1">Показать текст</button>
-
-<br>
+<button class="link" @click="toggle1">{{visible1}}</button>
 
 <p class="box" v-if="visible2">
   Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Обеспечивает переписали запятых свою имени журчит рыбными то своих семантика безорфографичный что коварных запятой буквоград строчка над, большого залетают. Журчит.
 </p>
-<button class="link" @click="showItem2">Показать текст</button>
-<button class="link" @click="hideItem2">Скрыть текст</button>
+<button class="link" @click="toggle2">{{visible2}}</button>
 
-<br>
-
-<p class="box" v-if="visible2">
+<p class="box" v-if="visible3">
   Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Обеспечивает переписали запятых свою имени журчит рыбными то своих семантика безорфографичный что коварных запятой буквоград строчка над, большого залетают. Журчит.
 </p>
-<button class="link" @click="showItem2" v-if="!visible2">Показать текст</button>
-<button class="link" @click="hideItem2" v-if="visible2">Скрыть текст</button>
+<button class="link" @click="toggle3">{{visible3}}</button>
+
 
 </template>
 
@@ -66,6 +62,7 @@ body {
   border-radius: 40px;
   transition: all 0.3s ease-out;
   margin: 20px;
+  display: block;
 }
 
 .link:hover {
