f741dd12a171d23282cc5e1f2a81106d52e801a6
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-11 16:35:23 +0300

    Цикл в диапазоне в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -3,29 +3,6 @@ export default {
   name: 'App',
   data() {
     return {
-      hrefs: [
-        { href: '1.html', text: 'text1' },
-        { href: '2.html', text: 'text2' },
-        { href: '3.html', text: 'text3' },
-      ],
-
-      products: [
-        {
-          name: 'product1',
-          price: 100,
-          quantity: 5
-        },
-        {
-          name: 'product2',
-          price: 200,
-          quantity: 4
-        },
-        {
-          name: 'product3',
-          price: 300,
-          quantity: 3
-        },
-      ]
     }
   },
   computed: {
@@ -37,74 +14,42 @@ export default {
 </script>
 
 <template>
+  <div class="wrapper">
+    <div class="box" v-for="num in 30">
+      <h1 class="title">{{ num }}</h1>
+      <p>
+        Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Журчит ты реторический
+        строчка безорфографичный скатился взгляд заголовок лучше встретил алфавит букв, до предупредила о, подпоясал над
+        которое если щеке.
+      </p>
+    </div>
+  </div>
 
-  <nav>
-    <ul class="menu_item">
-      <li class="menu-list" v-for="elem in hrefs">
-        <a :href="elem.href" class="menu-link">{{ elem.text }}</a>
-      </li>
-    </ul>
-  </nav>
 
-  <table>
-    <tr v-for="elem in products">
-      <td>{{ elem.name }}</td>
-      <td>{{ elem.price }}</td>
-      <td>{{ elem.quantity }}</td>
-    </tr>
-  </table>
 
 </template>
 
 <style>
-nav {
-  margin-top: 20px;
-}
-
-.menu_item {
-  display: flex;
-  justify-content: center;
-}
-
-.menu-list {
-  list-style: none;
-  margin-right: 10px;
-  font-weight: bold;
-}
-
-.menu-link {
-  text-decoration: none;
-  color: #ffffff;
-  background-color: #16A177;
-  border: 1px solid #16A177;
-  padding: 10px 25px;
-  transition: all 0.4s ease-out;
-  border-radius: 10px;
-}
-
-.menu-link:hover {
-  background-color: #fff;
-  border: 1px solid #16A177;
-  color: #16A177;
-}
-
-tr,
-td,
-th {
+.box {
+  width: 300px;
+  padding: 15px 10px;
   border: 1px solid #000;
+  background-color: #fff;
+  margin: 10px;
+  text-align: justify;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-table {
-  width: 50%;
-  text-align: center;
-  margin-top: 50px;
+.wrapper {
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
 }
 
-th {
-  padding: 10px;
-}
-
-td {
-  padding: 5px 7px;
+.title {
+  font-weight: bold;
+  font-size: 30px;
 }
 </style>
