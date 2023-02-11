e865d46785c8b41f09c1235a405587a35aa1e316
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-11 16:58:14 +0300

    Атрибут key в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -3,7 +3,22 @@ export default {
   name: 'App',
   data() {
     return {
-      items: [1, -2, 3, -4, 5],
+      products: [
+        {
+          id: 2,
+          name: 'product1',
+        },
+
+        {
+          id: 3,
+          name: 'product2',
+        },
+
+        {
+          id: 1,
+          name: 'product3',
+        },
+      ]
     }
   },
   computed: {
@@ -16,26 +31,45 @@ export default {
 
 <template>
 
-  <template v-for="elem in items">
-    <nav>
-      <ul v-if="elem > 0">
-        <li class="box">{{ elem }}</li>
-      </ul>
-    </nav>
-  </template>
+  <nav>
+    <ul class="menu_item">
+      <li class="menu-list" v-for="elem in products" :key="products.id">
+        <a href="#" class="menu-link active">{{ elem.name }}</a>
+      </li>
+    </ul>
+  </nav>
 
 </template>
 
 <style>
-.box {
-  width: 300px;
-  padding: 15px 10px;
-  border: 1px solid #000;
-  background-color: #fff;
-  margin: 10px;
-  text-align: justify;
+nav {
+  margin-top: 40px;
+}
+
+.menu_item {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
+}
+
+.menu-list {
+  list-style: none;
+  margin-right: 10px;
+  font-weight: bold;
+}
+
+.menu-link {
+  text-decoration: none;
+  color: #ffffff;
+  background-color: #16A177;
+  border: 1px solid #16A177;
+  padding: 10px 25px;
+  transition: all 0.4s ease-out;
+  border-radius: 10px;
+}
+
+.menu-link:hover {
+  background-color: #fff;
+  border: 1px solid #16A177;
+  color: #16A177;
 }
 </style>
