c6528c36009144292cce2a34517a355b72a38b92
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-20 15:36:31 +0300

    Атрибут class в фреймворке Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,61 +2,28 @@
 export default {
   data() {
     return {
-
+      cssClass: 'block',
     }
   },
   methods: {
-
   }
 }
 </script>
 
 <template>
-  <div class="wrapper">
-    <h1 class="title">Andrey Kuropatkin</h1>
-
-  </div>
-  <div class="block">
-    <img
-      src="https://camo.githubusercontent.com/1bf26edd9b7797ce93e3127ccc2c113de3383d2f12900e7e8662d077b292a200/68747470733a2f2f63646e2e666973686b692e6e65742f75706c6f61642f706f73742f323032312f30332f30392f333635303634392f362d31372e676966"
-      alt="">
-  </div>
-
-
+  <p :class="cssClass">
+    Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вопроса заголовок, он рукопись
+    маленький за предупредила журчит путь образ сбить лучше! Текст реторический, большой она жаренные lorem напоивший от
+    всех.
+  </p>
 </template>
 
 <style>
-body {
-  background-color: #000;
-}
-
 .block {
-  width: 800px;
-  margin: 0 auto;
-  padding-top: 50px;
-}
-
-img {
-  width: 800px;
-  height: 400px;
-  margin: 0 auto;
-
-}
-
-.title {
-  text-align: center;
-  font-size: 30px;
-  background-color: #f3970c;
-  margin-top: 100px;
-  border-radius: 20px;
-  box-shadow: 1px 1px 55px  rgba(246, 11, 0, 0.55);
-  font-family: arial;
-  padding: 10px 5px;
-}
-
-.wrapper {
+  border: 1px solid black;
+  padding: 20px;
   width: 300px;
-  margin: 0 auto;
+  margin: 20px;
+  text-align: justify;
 }
-
 </style>
