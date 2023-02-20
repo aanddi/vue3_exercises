72433b82003e723cde782abb613e9cd3147bfaae
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-20 15:50:06 +0300

    Реактивность объекта с CSS классами в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -4,11 +4,21 @@ export default {
     return {
       obj: {
         block: true,
-        selected: false,
+        red: false,
+      },
+      obj1: {
+        block: true,
+        toggle: false,
       },
     }
   },
   methods: {
+    setRed: function () {
+      this.obj.red = !this.obj.red;
+    },
+    toggle: function () {
+      this.obj1.toggle = !this.obj1.toggle;
+    }
   }
 }
 </script>
@@ -19,6 +29,14 @@ export default {
     маленький за предупредила журчит путь образ сбить лучше! Текст реторический, большой она жаренные lorem напоивший от
     всех.
   </p>
+  <button class="btn" @click="setRed">{{obj.red}}</button>
+
+  <p :class="obj1">
+    Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вопроса заголовок, он рукопись
+    маленький за предупредила журчит путь образ сбить лучше! Текст реторический, большой она жаренные lorem напоивший от
+    всех.
+  </p>
+  <button class="btn" @click="toggle">{{obj1.toggle}}</button>
 </template>
 
 <style>
@@ -30,9 +48,21 @@ export default {
   text-align: justify;
 }
 
-.selected {
+.red {
   background-color: red;
   color: #fff;
+}
+
+.btn {
+  background-color: #d80808;
+  color: #fff;
+  padding: 15px;
+  margin-left: 100px;
+  border-radius: 10px;
+}
+
+.toggle {
+  display: none;
 }
 
 </style>
