b321db361274735e3ecca556858d06dd384f3d0f
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-20 15:40:00 +0300

    Объект с CSS классами в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,7 +2,10 @@
 export default {
   data() {
     return {
-      cssClass: 'block',
+      obj: {
+        block: true,
+        selected: false,
+      },
     }
   },
   methods: {
@@ -11,7 +14,7 @@ export default {
 </script>
 
 <template>
-  <p :class="cssClass">
+  <p :class="obj">
     Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вопроса заголовок, он рукопись
     маленький за предупредила журчит путь образ сбить лучше! Текст реторический, большой она жаренные lorem напоивший от
     всех.
@@ -26,4 +29,10 @@ export default {
   margin: 20px;
   text-align: justify;
 }
+
+.selected {
+  background-color: red;
+  color: #fff;
+}
+
 </style>
