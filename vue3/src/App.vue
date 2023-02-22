93858dd673ea40082a3c0e1e34c3a4748dfc74e3
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-22 16:17:11 +0300

    Блокировка элементов в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,79 +2,36 @@
 export default {
   data() {
     return {
-      options: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-      selected: '',
-      selected1: '',
-      selected2: '',
-      selected3: '',
-      selected4: '',
-      selected5: '',
-      selected6: '',
-      
-      day: '',
-      month: '',
-      year: '',
-
-      options1: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-       '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
-        '25', '26', '27', '28', '29', '30', ' 31'],
-      options2: ['Январь', 'Февраль','Март', 'Апрель', 'Май','Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-      options3: ['2023', '2022','2021', '2020', '2019','2018']
+      isDisabled: true,
+      isDisabled1: true,
+      checked: false,
     }
   },
   methods: {
-    today: function () {
-      const date = new Date();
-      this.day = String(date.getDate());
-      this.month = String(date.getMonth() + 1);
-      this.year = date.getFullYear();
-    }
-    
+    toggle: function () {
+      this.isDisabled = false;
+    },
+    toggle1: function () {
+      this.isDisabled1 = false;
+    },
   }
 }
 </script>
 
 <template>
   <div class="wrapper">
-    <div class="block">
-      <select v-model="selected">
-        <option v-for="i in options">{{ i }}</option>
-      </select>
-      <p>{{ selected }}</p>
-    </div>
+    <input type="text" class="block_input" placeholder="andrey" v-bind:disabled="isDisabled">
+    <button class="btn" @click="toggle">btn</button>
 
-    <div class="block">
-      <select v-model="selected1">
-        <option v-for="i in options1">{{ i }}</option>
-      </select>
-      <select v-model="selected2">
-        <option v-for="i in options2">{{ i }}</option>
-      </select>
-      <select v-model="selected3">
-        <option v-for="i in options3">{{ i }}</option>
-      </select>
-      <p>{{ selected1 + ' ' + selected2 + ' ' + selected3 }}</p>
-    </div>
-
-    <div class="block">
-      <select v-model="selected4">
-        <option>{{day}}</option>
-      </select>
-      <select v-model="selected5">
-        <option>{{month}}</option>
-      </select>
-      <select v-model="selected6">
-        <option>{{year}}</option>
-      </select>
-      <button @click="today" class="btn">Today</button>
-      <p>{{ selected4 + '.' + selected5 + '.' + selected6 }}</p>
-  </div>
+    <input type="text" class="block_input" placeholder="kuropatkin" v-bind:disabled="isDisabled1">
+    <input type="checkbox" class="block_input " v-model="checked" @click="toggle1">
+    <button class="btn" @click="toggle">btn</button> 
   </div>
 </template>
 
 <style>
 .wrapper {
-  display: flex;
+  
 }
 
 label {
@@ -98,15 +55,7 @@ label {
   padding: 5px;
   border: 1px solid black;
   border-radius: 10px;
-}
-
-select {
-  border: 1px solid black;
-  margin-top: 10px;
-}
-
-p {
-  margin-top: 10px;
+  display: block;
 }
 
 .btn {
@@ -116,6 +65,7 @@ p {
   border: 1px solid #fd0606;
   transition: all 0.4s ease-out;
   margin-top: 10px;
+  display: block;
 }
 
 .btn:hover {
