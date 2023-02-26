7c5d8e2cc646b7372dc7e231cf82449982cda3c0
aanddi <kuropatkinn.a.i21@gmail.com>
2023-02-27 00:17:30 +0300

    Кнопка для удаления из массива объектов в Vue

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,72 +2,81 @@
 export default {
   data() {
     return {
-      arr: ['a', 'b', 'c', 'd']
+      users: [
+        {
+          id: 1,
+          name: 'name1',
+          salary: 100,
+          age: 30,
+        },
+        {
+          id: 2,
+          name: 'name2',
+          salary: 200,
+          age: 40,
+        },
+        {
+          id: 3,
+          name: 'name3',
+          salary: 300,
+          age: 50,
+        },
+      ],
     }
   },
   methods: {
-    removeItem: function (index)  {
-      this.arr.splice(index, 1)
+    removeUser: function (id) {
+      this.users = this.users.filter((user) => {
+        return user.id !== id;
+      })
     }
   }
 }
 </script>
 
 <template>
-  <ul>
-    <li class="block" @click="removeItem(index)" v-for="(item, index) in arr" :key="index">
-      {{ item }}
-    </li>
-  </ul>
-
- 
+  <table class="tb">
+    <tr v-for="user in users" :key="user.id">
+      <td class="block">
+        <td class="block">{{ user.name }}</td> 
+        <td class="block">{{ user.salary }}</td>
+        <td class="block">{{ user.age }}</td>  
+        <td><button class="btn" @click="removeUser(user.id)">remove</button></td>
+      </td>
+    </tr>
+  </table>
 </template>
 
 <style>
+.tb {
+  margin: 0 auto;
+  border: 1px solid black;
+  border-radius: 30px;
+  padding: 20px;
+  margin-top: 40px;
+}
+
 .block {
   border: 1px solid black;
-  padding: 30px 30px;
-  width: 300px;
-  margin: 20px 10px;
-  text-align: justify;
+  padding: 10px;
+  margin: 10px;
   display: flex;
-  justify-content: center;
-  flex-direction: column;
-}
-
-.block:hover{
-  background-color: #000;
-  color: #fff;
-}
-
-.block_input {
-  width: 200px;
-  margin: 10px 10px;
-  padding: 5px;
-  border: 1px solid black;
-  border-radius: 10px;
-  display: block;
+  border-radius: 3px;
 }
 
 .btn {
   display: block;
-  width: 200px;
-  text-align: center;
-  background-color: #fd0606;
-  padding: 10px 40px;
-  border-radius: 20px;
-  border: 1px solid #fd0606;
+  padding: 10px;
+  background-color: black;
+  border: 1px solid black;
+  color: #fff;
   transition: all 0.4s ease-out;
-  margin: 20px 10px;
+  margin: 10px;
 }
 
 .btn:hover {
   background-color: #fff;
   color: #fd0606;
   border: 1px solid #fd0606;
-}
-
-ul {
-  margin-left: 20px;
 }
 </style>
